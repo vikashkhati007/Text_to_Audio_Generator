19e51f2684af7f43ad780bd8f5b4cfa42383819b
Vikash Khati <124573409+vikashkhati007@users.noreply.github.com>
2023-09-17 20:33:34 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
-import Input from "@/components/Input"
-import Navbar from "@/components/Navbar"
-import HomeBody from "@/components/HomeBody"
+import Input from "../components/Input"
+import Navbar from "../components/Navbar"
+import HomeBody from "../components/HomeBody"
 import { useState } from "react"
 export default function Home() {
   const [userinput, setUserInput] = useState("");
